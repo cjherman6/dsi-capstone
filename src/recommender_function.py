f2a41be863a0e0dc40dacd89f4316efa717cefee
Christopher Herman <Chris@Christophers-MacBook-Air.local>
2018-12-13 12:51:43 -0700

    moved files from ec2 to local repo, working on flask upload function

---
+++ src/recommender_function.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import euclidean_distances,cosine_similarity
+
+df = pd.read_csv('breed_traits.csv',index_col='Unnamed: 0')
+
+def overall_recommender(breed,dist='euclidean'):
+    '''
+    Input: Name of breed (string)
+    Output: 5 Breeds with most similar temperaments according to dogtime.com ratings
+    '''
+    y = df.loc[[breed],:]
+    euc_dists = euclidean_distances(df.values,y.values)
+    euc_ind = np.argsort(euc_dists.flatten())
+    cos_dists = cosine_similarity(df.values,y.values)
+    cos_ind = np.argsort(cos_dists.flatten())
+    if dist == 'euclidean':
+        return [df.iloc[ind,:].name for ind in euc_ind][1:6]
+    elif dist == 'cosine':
+        return [df.iloc[ind,:].name for ind in cos_ind][1:6]
+
+def predictions_recommender(breed,photo_list,dist='euclidean'):
+    '''
+    Input: Name of breed (string), List of dogs you're considering (list)
+    Output: Ordered list starting from most similar to least
+    '''
+    y = df.loc[[breed],:]
+    X = df.loc[photo_list,:]
+    euc_dists = euclidean_distances(X.values,y.values)
+    euc_ind = np.argsort(euc_dists.flatten())
+    cos_dists = cosine_similarity(X.values,y.values)
+    cos_ind = np.argsort(cos_dists.flatten())
+    if dist == 'euclidean':
+        return [X.iloc[ind,:].name for ind in euc_ind]
+    elif dist == 'cosine':
+        return [X.iloc[ind,:].name for ind in cos_ind]
